1935e7faffc17369b2f3c193203f460a157850f9
Victor Barbosa <126208560+VictorAuBarbosa@users.noreply.github.com>
2023-08-16 14:58:49 -0300

    Add files via upload

---
+++ Perfil/perfil.js
@@ -0,0 +1,26 @@
+// Obtém todos os elementos da div navmenu
+const navmenuItems = document.querySelectorAll('.navmenu > div');
+
+// Obtém todas as seções
+const sections = document.querySelectorAll('.all > div');
+
+// Adiciona um ouvinte de evento de clique a cada elemento da div navmenu
+navmenuItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    // Remove a classe ativa de todos os elementos da div navmenu
+    navmenuItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    // Adiciona a classe ativa ao elemento clicado
+    item.classList.add('active');
+
+    // Oculta todas as seções
+    sections.forEach((section) => {
+      section.style.display = 'none';
+    });
+
+    // Exibe a seção correspondente ao índice do elemento clicado
+    sections[index].style.display = 'block';
+  });
+});
